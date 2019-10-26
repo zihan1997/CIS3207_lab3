fd0091cca5b46fdf1acc153bc1df15ba2f40b644
BlueBug <zihan9701@gmail.com>
2019-10-26 10:27:35 -0400

    add funcs enqueue

--- Queue.c
+++ Queue.c
@@ -7,23 +7,40 @@ struct log
 };
 
 typedef struct{
-    int front;
-    int tail;
+    int size;
     struct log queue[MAX];
 }queue_log;
 
 typedef struct {
-    int front;
-    int tail;
+    int size;
     int queue[MAX];
 }queue_job;
 
 void init_queue_log(queue_log* log_queue){
-    log_queue->front = 0;
-    log_queue->tail = 0;
+    log_queue->size = 0;
 }
 
 void init_queue_job(queue_job* job_queue){
-    job_queue->front = 0;
-    job_queue->tail = 0;
+    job_queue->size = 0;
+}
+
+int enQueue_log(queue_log* log_queue, int socket){
+    // queue is full
+    if(log_queue->size == (MAX-1)){
+        return 0;
+    }
+    int *arr = log_queue->queue;
+    arr[log_queue->size] = socket;
+    log_queue->size += 1;
+    return 1;
+}
+int enQueue_job(queue_job* job_queue, int socket){
+    // queue is full
+    if(job_queue->size == (MAX-1)){
+        return 0;
+    }
+    int *arr = job_queue->queue;
+    arr[job_queue->size] = socket;
+    job_queue->size += 1;
+    return 1;
 }
