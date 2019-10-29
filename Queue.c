5cb9e43bdb6afa4e306e9b52a6dc68db1e7dcd3a
BlueBug <zihan9701@gmail.com>
2019-10-29 13:21:11 -0400

    fixed struct log part in funcs

--- Queue.c
+++ Queue.c
@@ -24,13 +24,13 @@ void init_queue_job(queue_job* job_queue){
     job_queue->size = 0;
 }
 
-int enQueue_log(queue_log* log_queue, struct log* logs){
+int enQueue_log(queue_log* log_queue, struct log* result){
     // queue is full
     if(log_queue->size == (MAX-1)){
         return 0;
     }
     int *arr = log_queue->queue;
-    arr[log_queue->size] = logs;
+    arr[log_queue->size] = result;
     log_queue->size += 1;
     return 1;
 }
@@ -57,14 +57,16 @@ void queueAdjust(int *arr, int *size)
     *size -= 1;
 }
 
-int deQueue_log(queue_log* log_queue, int *socket){
+int deQueue_log(queue_log* log_queue, struct log* result){
     // check if the queue if empty
     if(log_queue->size == 0){
         return 0;
     }else{
-        int *arr = log_queue->queue;
-        *socket = arr[0];
-        queueAdjust(arr, &log_queue->size);
+        // int *arr = log_queue->queue;
+        result = &log_queue->queue[0];
+        for(int i = 0; i < log_queue->size - 1; i++){
+            log_queue[i] = log_queue[i+1];
+        }
         return 1;
     }
 }
