578f258b9adb1e04adcb031d35b7285a4c16e9ed
BlueBug <zihan9701@gmail.com>
2019-11-10 14:30:30 -0500

    multi clients

--- Multi_Client.c
+++ Multi_Client.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <pthread.h>
@@ -15,64 +16,77 @@
 #define MAX_WORKERS 10
 
 
-queue_job connects;
+queue_log results;
 pthread_mutex_t mutex;
 pthread_cond_t fill, empty;
 
 unsigned int port;
 
 void* clients(void* id){
+    // common struct
+    struct sockaddr_in serveradr;
+    serveradr.sin_family = AF_INET;
+    serveradr.sin_port = htons(port);
+
+    // get binary form of IP
+    if( inet_pton(AF_INET, "127.0.0.1", &serveradr.sin_addr) <= 0 ){
+        perror("Invalid IP addr\n");
+        return 0;
+    }
+
     while(1){
-        int clientSocket;
-        // Consumers
         pthread_mutex_lock(&mutex);
-            while(connects.size == 0){
-                pthread_cond_wait(&fill, &mutex);
-            }
-        printf("Thread%d ready\n", *(int *)id);
-        deQueue_job(&connects, &clientSocket);
+        while(results.size == 0){
+            pthread_cond_wait(&fill, &mutex);
+        }
+        printf("ID> %d\n", *(int *)id);
+        struct log w;
+        deQueue_log(&results, &w);
         pthread_cond_signal(&empty);
         pthread_mutex_unlock(&mutex);
-        // connect
-        struct sockaddr_in serveradr;
-        serveradr.sin_family = AF_INET;
-        serveradr.sin_port = htons(port);
-        
+
+        int sockets = socket(AF_INET, SOCK_STREAM, 0);
+        if(sockets < 0){
+            perror("failed to create socket\n");
+            return 0;
+        }
         // get binary form of IP
         if( inet_pton(AF_INET, "127.0.0.1", &serveradr.sin_addr) <= 0 ){
             perror("Invalid IP addr\n");
-            return -1;
+            return 0;
         }
-
-        if( connect(clientSocket, (struct sockaddr *)&serveradr, sizeof(serveradr)) < 0){
+        if( connect(sockets, (struct sockaddr *)&serveradr, sizeof(serveradr)) < 0){
             perror("failed to connet\n");
-            return -1;
+            return 0;
         }
+        // For debug
+            // puts("connected");
+            // printf("Sending word %s\n", w.word);
+        send(sockets, w.word, sizeof(w.word), 0);
         char buffer[BUFFER_SIZE] = "\0";
-        while(1){
-            fflush(stdin);
-            fflush(stdout);
-            printf("Word> ");
-            scanf("%s", buffer);
-            send(clientSocket, buffer, sizeof(buffer), 0);
-            // escape char
-            if(buffer[0] == 27){
-                close(clientSocket);
-                break;
-            }
-            recv(clientSocket, buffer, sizeof(buffer), 0);
-            // printf("received buffer:\n%s\n", buffer);
-            printf("%s\n", buffer);
-            
-        }
+        recv(sockets, buffer, sizeof(buffer), 0);
+        printf("%s\n", buffer);
+        puts("finished");
+
+        // Close connect
+        char exitMsg[2];
+        exitMsg[0] = 27;
+        exitMsg[1] = '\0';
+        send(sockets, exitMsg, sizeof(exitMsg), 0);
+        close(sockets);
     }
     return 0;
 }
 
 int main(int argc, char const *argv[])
 {
+    // Port get
+    port = DEFAULT_PORT;
+    if(argc == 2){
+        port = atoi(argv[1]);
+    }
     // Initial Queues
-    init_queue_job(&connects);
+    init_queue_log(&results);
 
     // Initial cond_t mutex
     pthread_cond_init(&empty, 0);
@@ -87,38 +101,26 @@ int main(int argc, char const *argv[])
         pthread_create(&threads[i], NULL, &clients, &ids[i]);
     }
 
-    // Port get
-    port = DEFAULT_PORT;
-    if(argc == 2){
-        port = atoi(argv[1]);
-    }
-    // Socket implement
-    int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
-    if(clientSocket < 0){
-        perror("failed to create socket\n");
-        return -1;
-    }
-
-    int clientID;
-    while(1){
-        printf("ID: ");
-        scanf("%d", &clientID);
-        printf("Entered %d\n", clientID);
-        if(clientID > 2){
-            continue;
-        }
-        if(clientID == -1){
+    puts("finish init");
+    char word[20];
+    do{
+        sleep(1);
+        printf("> ");
+        scanf("%s", word);
+        if(word[0] == 27){
             break;
         }
         pthread_mutex_lock(&mutex);
-            while(connects.size == MAX){
-                pthread_cond_wait(&empty, &mutex);
-            }
-        enQueue_job(&connects, clientSocket);
+        while(results.size == MAX){
+            pthread_cond_wait(&empty, &mutex);
+        }
+        struct log w;
+        strcpy(w.word, word);
+        strcpy(w.status, "null");
+        enQueue_log(&results, w);
         pthread_cond_signal(&fill);
         pthread_mutex_unlock(&mutex);
-    }
-    
-    
+    }while(word[0] != 27);
+
     return 0;
 }
